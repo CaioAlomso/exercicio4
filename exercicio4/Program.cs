805a3a7f4f929d811c06e31a10e005007ddaa1b7
pedro <pedro@DESKTOP-R8CBM62>
2022-07-06 18:40:32 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int v = 1; v <= 50; v++)
+            {
+                Console.WriteLine(v.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
